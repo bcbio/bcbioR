9c7bd8736389d8a5345e18112cc293391c8c6d7e
Lorena Pantano <lorena.pantano@gmail.com>
2024-04-16 12:46:07 -0400

    a new beginning

---
+++ inst/rmarkdown/templates/rnaseq_qc/skeleton/params_de.R
@@ -0,0 +1,15 @@
+# info params
+project = "name_hbcXXXXX"
+PI = 'person name'
+experiment = 'short description'
+aim = 'short description'
+analyst = 'person in the core'
+
+# project params
+root = "../"
+date = "YYYYMMDD"
+column = "treatment"
+subset_column = 'cell'
+metadata_fn = "../meta/samplesheet.csv"
+counts_fn = '../data/tximport-counts.csv'
+basedir <- 'reports'
