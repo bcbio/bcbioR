3a7dee4e77de04f74d132d51b04fabb85773ecc5
Alex Bartlett <albartlett@hsph.harvard.edu>
2024-04-22 17:32:58 -0400

    use test data, start adding emma code

--- inst/rmarkdown/templates/rnaseq/skeleton/params_de.R
+++ inst/rmarkdown/templates/rnaseq/skeleton/params_de.R
@@ -8,8 +8,8 @@ analyst = 'person in the core'
 # project params
 root = "../"
 date = "YYYYMMDD"
-column = "treatment"
-subset_column = 'cell'
-metadata_fn = "../meta/samplesheet.csv"
-counts_fn = '../data/tximport-counts.csv'
+column = "sample_type"
+subset_column = NA
+metadata_fn = "/n/data1/cores/bcbio/platform/test-data/rnaseq/GSE251845_colon_n3_tumor_normal/final/2024-04-18_samples/coldata.csv"
+counts_fn = '/n/data1/cores/bcbio/platform/test-data/rnaseq/GSE251845_colon_n3_tumor_normal/final/2024-04-18_samples/counts/tximport-counts.csv'
 basedir <- 'reports'
---
+++ inst/rmarkdown/templates/rnaseq/skeleton/params_qc.R
@@ -0,0 +1,11 @@
+# info params
+project = "Interon_rnaseq_hbc04916"
+PI = 'Donggi Paik, Ph.D.'
+experiment = '8 human cell lines from multiple origins. Treatment are 9 conditions including untreated.'
+aim = 'Determine treatment effect within cell line and in combination with TNF'
+analyst = 'Lorena Pantano, PhD'
+
+
+metadata_fn="../meta/hbc04916_metadata_bcbio.csv"
+se_object="../counts/bcbio-se.rds"
+cols_to_show=c("cell","treatment")
