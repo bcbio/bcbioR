15e3e16ac8dab7d5cfd289d78c3465015ed7b373
Alex Bartlett <albartlett@hsph.harvard.edu>
2024-05-17 11:38:15 -0400

    clean up cosmx template

--- inst/rmarkdown/templates/cosmx/skeleton/QC/run_markdown.R
+++ inst/rmarkdown/templates/cosmx/skeleton/QC/run_markdown.R
@@ -8,7 +8,6 @@ rmarkdown::render("reports/QC.Rmd",
                   params = list(
                     seurat_fn = '../data/CTCL_PAT8_DEV3_LVL7.RDS',
                     project_file = './information.R',
-                    umap_dim = 'approximateumap_8c6f278e.b9f4.4535.aeca.8955c1dff614_1',
-                    image_fn = 'LVL7.png'
+                    umap_dim = 'approximateumap_8c6f278e.b9f4.4535.aeca.8955c1dff614_1'
                     )
 )
