b9a3c0556be045231b2f2bce32095a0694b53b95
Lorena Pantano <lorena.pantano@gmail.com>
2024-07-31 18:05:29 -0400

    try to fix names in rnaseq

---
+++ inst/templates/rnaseq/QC/params_qc.R
@@ -0,0 +1,4 @@
+# info params
+
+metadata_fn='https://raw.githubusercontent.com/bcbio/bcbioR-test-data/main/rnaseq/bcbio/coldata.csv'
+se_object=url("https://raw.githubusercontent.com/bcbio/bcbioR-test-data/main/rnaseq/bcbio/bcbio-se.rds")
---
+++ inst/templates/rnaseq/QC/params_qc_nf-core.R
@@ -0,0 +1,11 @@
+# info params
+
+# Your data
+# This is the file used to run nf-core or compatible to that
+metadata_fn='/Path/to/metadata/meta.csv'
+# This file is inside star_salmon/ folder
+se_object='/path/to/nf-core/output/star_salmon/salmon.merged.gene_counts.rds'
+# This folder called "multiqc_report_data" is inside the output directory star_salmon inside multiqc folder
+multiqc_data_dir='/path/to/nf-core/output/multiqc/star_salmon/multiqc_report_data'
+# This file is inside the genome folder in the output directory, use this only for non-model organism
+# gtf_fn='/path/to/nf-core/output/genome/hg38.filtered.gtf'
