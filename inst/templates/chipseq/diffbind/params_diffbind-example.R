f517d42d7c4fc4080f7a0fb45bc131fdb659f100
Alex Bartlett <albartlett@hsph.harvard.edu>
2024-09-19 13:56:33 -0400

    begin diffbind templates

---
+++ inst/templates/chipseq/diffbind/params_diffbind-example.R
@@ -0,0 +1,8 @@
+# info params
+
+
+# Example data
+coldata_fn='chipseq_peakanalysis_H3K27Ac.csv'
+peaks_dir = '~/s3_results/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/macs2/narrowPeak/'
+bam_dir = '~/s3_results/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/'
+peak_caller = 'macs'
---
+++ inst/templates/chipseq/libs/make_diffbind_samplesheet.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(tools)
+
+source('../libs/load_data.R')
+source('params_diffbind-example.R')
+
+coldata <- load_coldata(coldata_fn)
+
+bam_files <- data.frame(bam = list.files(bam_dir, pattern = '.bam$', full.names = T)) %>%
+  mutate(sample = sub("\\..*", "",basename(bam)))
+
+peak_files <- data.frame(Peaks = list.files(peaks_dir, pattern = 'Peak$', full.names = T)) %>%
+  mutate(SampleID = sub("\\..*", "",basename(Peaks))) %>%
+  mutate(SampleID = gsub('_peaks', '', SampleID))
+
+coldata_for_diffbind <- coldata %>% 
+  filter(!is.na(control) & control != '') %>%
+  # select(-description) %>%
+  dplyr::rename(ControlID = control, SampleID = sample) %>% 
+  separate(SampleID, into = c('sample', 'Replicate'), remove = F, sep = '_REP') %>%
+  mutate(peakCaller = peak_caller) 
+
+samplesheet <- coldata_for_diffbind %>%
+  left_join(bam_files %>% select(SampleID = sample, bamReads = bam), by = 'SampleID') %>%
+  left_join(bam_files %>% select(ControlID = sample, bamControl = bam), by = 'ControlID') %>%
+  left_join(peak_files, by = 'SampleID')
