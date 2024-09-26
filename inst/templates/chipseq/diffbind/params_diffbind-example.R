b7201baff5dfb965dfc8a221ec9c8303b8dbc148
Alex Bartlett <albartlett@hsph.harvard.edu>
2024-09-26 17:36:14 +0000

    full report

--- inst/templates/chipseq/diffbind/params_diffbind-example.R
+++ inst/templates/chipseq/diffbind/params_diffbind-example.R
@@ -2,6 +2,6 @@
 
 
 # Example data
-coldata_fn='/workspace/s3_hcbc_seqera/test-data/chipseq_peakanalysis_h3k27ac_narrow/chipseq_peakanalysis_H3K27Ac.csv'
-peaks_dir = '/workspace/s3_hcbc_seqera/test-data/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/macs2/narrowPeak/'
-bam_dir = '/workspace/s3_hcbc_seqera/test-data/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/'
+coldata_fn='/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/chipseq_peakanalysis_H3K27Ac.csv'
+peaks_dir = '/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/macs2/narrowPeak/'
+bam_dir = '/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/'
