255147933c10f0ed27f545b5cbd1461b114dcc83
Alex Bartlett <albartlett@hsph.harvard.edu>
2024-10-01 18:27:23 +0000

    add non-example params files

---
+++ inst/templates/chipseq/diffbind/params_diffbind.R
@@ -0,0 +1,9 @@
+# info params
+
+coldata_fn='/path/to/nf-core/samplesheet.csv'
+
+# This folder is in the nf-core output directory, maybe is broadPeak instead of narrowPeak
+peaks_dir = '/path/to/nf-core/output/bowtie2/mergedLibrary/macs2/narrowPeak/'
+
+# This folder is in the nf-core output directory
+bam_dir = '/path/to/nf-core/output/bowtie2/mergedLibrary/'
