d292490d3eacd1b10fc52e1e92a7ffd8cbb69fdc
Alex Bartlett <albartlett@hsph.harvard.edu>
2024-10-24 20:59:23 +0000

    diffbind example data in github

--- inst/templates/chipseq/diffbind/params_diffbind-example.R
+++ inst/templates/chipseq/diffbind/params_diffbind-example.R
@@ -1,7 +1,14 @@
 # info params
-
-
 # Example data
-coldata_fn='/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/chipseq_peakanalysis_H3K27Ac.csv'
-peaks_dir = '/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/macs2/narrowPeak/'
-bam_dir = '/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/'
+
+# this is the samplesheet used to run nf-core, with additional columns containing covariates of interest
+coldata_fn='https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/chipseq/chipseq_peakanalysis_H3K27Ac.csv'
+
+# example data doesn't need this but this folder is in the nf-core output directory, maybe is broadPeak instead of narrowPeak
+# peaks_dir = '/path/to/nf-core/output/bowtie2/mergedLibrary/macs2/narrowPeak/'
+
+# example data doesn't need this but this folder is in the nf-core output directory
+# bam_dir = '/path/to/nf-core/output/bowtie2/mergedLibrary/'
+
+diffbind_samplesheet_fn = 'https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/chipseq/diffbind_samplesheet.csv'
+diffbind_counts_fn = 'https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/chipseq/diffbind_counts.qs'
--- inst/templates/chipseq/diffbind/params_diffbind.R
+++ inst/templates/chipseq/diffbind/params_diffbind.R
@@ -7,3 +7,9 @@ peaks_dir = '/path/to/nf-core/output/bowtie2/mergedLibrary/macs2/narrowPeak/'
 
 # This folder is in the nf-core output directory
 bam_dir = '/path/to/nf-core/output/bowtie2/mergedLibrary/'
+
+# this will be the file that the diffbind samplesheet is eventually saved in 
+diffbind_samplesheet_fn = 'diffbind_samplesheet.csv'
+
+# This will be the file that the diffbind counts matrix is eventually saved in
+diffbind_counts_fn = 'diffbind_counts.qs'
