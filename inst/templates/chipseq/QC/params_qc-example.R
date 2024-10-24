12d00d353a38ae46fc3237d17fb768ab89e6d509
Alex Bartlett <albartlett@hsph.harvard.edu>
2024-10-24 19:48:02 +0000

    QC test data working from github

--- inst/templates/chipseq/QC/params_qc-example.R
+++ inst/templates/chipseq/QC/params_qc-example.R
@@ -1,11 +1,14 @@
 # info params
-
-
 # Example data
-coldata_fn='/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/chipseq_peakanalysis_H3K27Ac.csv'
+
+# this is the samplesheet used to run nf-core, with additional columns containing covariates of interest
+coldata_fn='https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/chipseq/chipseq_peakanalysis_H3K27Ac.csv'
+
 # This folder is in the output directory inside multiqc folder
-multiqc_data_dir='/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/multiqc/narrowPeak/multiqc_data/'
-# This folder is in the output director
-peaks_dir = '/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/macs2/narrowPeak/'
+multiqc_data_dir='https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/chipseq/multiqc/narrowPeak/multiqc_data/' 
+
+# This folder is in the output directory
+peaks_dir = "https://api.github.com/repos/bcbio/bcbioR-test-data/contents/chipseq/bowtie2/mergedLibrary/macs2/narrowPeak"
+
 # This folder is in the output directory 
-counts_fn = '/workspace/data/chipseq_peakanalysis_h3k27ac_narrow/bowtie2/mergedLibrary/macs2/narrowPeak/consensus/H3K27ac/deseq2/H3K27ac.consensus_peaks.rds'
+counts_fn = url('https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/chipseq/bowtie2/mergedLibrary/macs2/narrowPeak/consensus/H3K27ac/deseq2/H3K27ac.consensus_peaks_small.rds')
