1d896e21f104e784c8b4360e34576a5c20ff7544
Elizabeth Partan <36081569+epartan@users.noreply.github.com>
2024-12-19 14:08:40 -0500

    Add immune deconvolution template

---
+++ inst/templates/rnaseq/03_functional/params_immune_deconv.R
@@ -0,0 +1,6 @@
+# gene expression matrix: raw counts
+input_counts="../counts/tximport-counts.csv"
+# gene expression matrix: TPM
+input_tpm="../counts/tximport-tpm.csv"
+# sample metadata
+metadata="metadata.csv"
